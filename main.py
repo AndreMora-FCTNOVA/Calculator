a49094e983c4f50a9994f50765bc54564d652434
Manuel Lagarto <rod.lagarto@hotmail.com>
2024-04-03 17:30:40 +0100

    [ADDED] Subtract(x,y) function.

--- main.py
+++ main.py
@@ -10,12 +10,17 @@ def mySqrt(x):
     """Function to square root"""
     return math.sqrt(x)
 
+def subtract(x, y):
+    """Function to subtract two numbers"""
+    return x - y
+
 
 
 
 print("Select operation:")
 print("1. Add")
 print("2. Sqrt")
+print("3. Subtract")
 
 
 
@@ -30,11 +35,21 @@ while True:
             print("Result:", add(num1, num2))
 
         break
+
     elif choice in ('2'):
         num1 = float(input("Enter the number: "))
 
         if choice == '2':
             print("Result:", mySqrt(num1))
+
+    elif choice in ('3'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == '3':
+            print("Result:", subtract(num1, num2))
+
+        break
 
     else:
         print("Invalid input. Please enter a valid number (1/2/3/4).")
