1730b423f170b26bae8816aff711153af3f2a4d5
LAPTOP-4LNJQ83I\Acer <miguel.loureiro.1095@gmail.com>
2024-04-03 17:36:11 +0100

    Added: Factorial function

--- main.py
+++ main.py
@@ -11,13 +11,18 @@ def mySqrt(x):
     return math.sqrt(x)
 
 
+def factorial(x):
+    result = x
+    for i in range(x-1):
+        print(result)
+        print(i+1)
+        result = result * (i+1)
+    return result
 
 
 print("Select operation:")
 print("1. Add")
 print("2. Sqrt")
-
-
 
 while True:
     choice = input("Enter choice (1- ADD MORE ): ")
@@ -35,6 +40,12 @@ while True:
 
         if choice == '2':
             print("Result:", mySqrt(num1))
+
+    elif choice in ('7'):
+        num1 = float(input("Enter the number: "))
+
+        if choice == '7':
+            print(("Result:", factorial(int(num1))))
 
     else:
         print("Invalid input. Please enter a valid number (1/2/3/4).")
