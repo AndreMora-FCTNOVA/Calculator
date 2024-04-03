0f53eb4d044ac2188fa476dac5db5d4368c883a3
kingak852 <264107@student.pwr.edu.pl>
2024-04-03 17:31:35 +0100

    Square calculation

--- main.py
+++ main.py
@@ -12,10 +12,16 @@ def mySqrt(x):
 
 
 
+def Square(x):
+    """Function to calculate the square of a number"""
+    return x ** 2
+
 
 print("Select operation:")
 print("1. Add")
 print("2. Sqrt")
+
+print("7. Square ")
 
 
 
@@ -36,5 +42,10 @@ while True:
         if choice == '2':
             print("Result:", mySqrt(num1))
 
+    elif choice in ('7'):
+        num1 = float(input("Enter the number: "))
+
+        if choice == '7':
+            print("Result:", Square(num1))
     else:
-        print("Invalid input. Please enter a valid number (1/2/3/4).")
+        print("Invalid input. Please enter a valid number (1/2/3/4/5/6/7).")
